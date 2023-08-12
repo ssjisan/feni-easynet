ea6cbc2dd43315e89520a4dedfa8bc0a069d7367
ssjisan <sakibfset13@gmail.com>
2023-08-12 14:46:39 +0600

    navbar down menu click

--- Plugins/dropdown.js
+++ Plugins/dropdown.js
@@ -1,8 +1,13 @@
 $(document).ready(function () {
-    $('#dropdown-hidden-menu').click(function () {
-      $('.dropdown-menu-on').slideToggle("slow");
-    });
-    $('.click').click(function(){
-      $('.dropdown-menu-on').slideToggle("slow");
-    })
+  $('#dropdown-hidden-menu').click(function () {
+    $('.dropdown-menu-on').slideToggle("slow");
   });
+  $('.click').click(function () {
+    $('.dropdown-menu-on').slideToggle("slow");
+  })
+});
+$(document).click(function (event) {
+  if (!$(event.target).closest('.nav-item.dropdown').length) {
+    $('.dropdown-menu-on').slideUp("slow");
+  }
+});
